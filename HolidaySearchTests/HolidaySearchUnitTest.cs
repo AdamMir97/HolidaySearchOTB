8751945b0f388672128e740c33417d11c88a3e71
AdamMir97 <adammir97@gmail.com>
2025-09-01 11:19:54 +0100

    Adds initial setup

---
+++ HolidaySearchTests/HolidaySearchUnitTest.cs
@@ -0,0 +1,20 @@
+namespace HolidaySearchTests
+{
+    public class HolidaySearchUnitTest
+    {
+        [Fact]
+        public void FlightMatchesWithHotel()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        public void SearchResultsAreOrderedByValue()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
