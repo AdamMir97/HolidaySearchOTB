72968a1e4aee2dd79ba0164c57b733a6c68a6ad9
AdamMir97 <adammir97@gmail.com>
2025-09-01 18:37:31 +0100

    Tied code into program.cs

---
+++ OTBHolidaySearch/HolidaySearch.cs
@@ -0,0 +1,52 @@
+﻿using OTBHolidaySearch.Models;
+using OTBHolidaySearch.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTBHolidaySearch
+{
+    public class HolidaySearch
+    {
+        private readonly IRepository<Flight> _flightRepo;
+        private readonly IRepository<Hotel> _hotelRepo;
+        private readonly HolidayMatchingService _matcher;
+
+        public HolidaySearch(IRepository<Flight> flightRepo, IRepository<Hotel> hotelRepo, HolidayMatchingService matcher)
+        {
+            _flightRepo = flightRepo;
+            _hotelRepo = hotelRepo;
+            _matcher = matcher;
+        }
+
+        public void Run()
+        {
+            var flights = _flightRepo.GetAll();
+            var hotels = _hotelRepo.GetAll();
+
+            string[] departingFrom = {"MAN" };
+            var flightDate = new DateTime(2023, 07, 01);
+            var duration = 7;
+
+            var service = _matcher;
+
+            var matches = service.Match(flights, hotels, departingFrom, flightDate, duration).ToList();
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine($"Total cost: {match.Item1.Cost + match.Item2.TotalCost} " + 
+                                  $"\nFlight Id: {match.Item1.Id} " +
+                                  $"\nDeparting from: {match.Item1.DepartingFrom} " +
+                                  $"\nArriving to: {match.Item1.DestinationName} " +
+                                  $"\nFlight Cost: {match.Item1.Cost} " +
+                                  $"\nHotel Id: {match.Item2.Id} " +
+                                  $"\nHotel Name: {match.Item2.Name} " +
+                                  $"\nHotel total Cost: {match.Item2.TotalCost} "
+                                  );
+            }
+
+        }
+    }
+}
--- OTBHolidaySearch/Program.cs
+++ OTBHolidaySearch/Program.cs
@@ -7,9 +7,18 @@ namespace OTBHolidaySearch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Run the tests to see it work
 
             var flightRepo = new JsonFlightRepository("JSONdata/flightdata.json");
             var hotelRepo = new JsonHotelRepository("JSONdata/hoteldata.json");
+
+            var matcher = new HolidayMatchingService();
+
+            var holidaySearch = new HolidaySearch(flightRepo, hotelRepo, matcher); 
+
+            holidaySearch.Run();
+
+            
         }
     }
 }
